1a80d3fd16819231640ac336f078231397ad90e1
Amit Patel <patelamit812@gmail.com>
2022-04-27 16:02:27 +0530

    my first commit

---
+++ src/main/java/com/codewithamit/blog/BlogsAppApisApplication.java
@@ -0,0 +1,13 @@
+package com.codewithamit.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsAppApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithamit/blog/BlogsAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithamit.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

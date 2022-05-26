92fb805dbac08b50bfa6b5afbfc6bdd2c053aeec
Amit Patel <patelamit812@gmail.com>
2022-05-26 19:42:34 +0530

    Implementing Searching in our Backend Application

--- src/main/java/com/codewithamit/blog/services/impl/PostServiceImpl.java
+++ src/main/java/com/codewithamit/blog/services/impl/PostServiceImpl.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -65,9 +66,11 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public PostResponse getAllPost(Integer pageNumber, Integer pageSize) {
+    public PostResponse getAllPost(Integer pageNumber, Integer pageSize,String sortBy, String sortDir) {
 
-        Pageable pageable = PageRequest.of(pageNumber,pageSize);
+        Sort sort = (sortBy.equalsIgnoreCase("asc")?Sort.by(sortBy).ascending():Sort.by(sortBy).descending());
+
+        Pageable pageable = PageRequest.of(pageNumber,pageSize,sort);
 
         Page<Post> pagePost = this.postRepo.findAll(pageable);
 
